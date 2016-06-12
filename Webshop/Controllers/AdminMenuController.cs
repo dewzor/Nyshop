68c5fb973c1d8fc13170d28b62966d7bbd19f672
dewzor <16418668+dewzor@users.noreply.github.com>
2016-06-12 12:18:28 +0200

    AdminEmployeeMenus

--- Webshop/Controllers/AdminMenuController.cs
+++ Webshop/Controllers/AdminMenuController.cs
@@ -1,29 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 
 namespace Webshop.Controllers
 {
-    //public class AdminMenuController : Controller
-    //{
-    //    [Authorize(Roles = "Admin")]
-    //    // GET: Admin
-    //    public ActionResult AdminMenu() // Make this Async task?
-    //    {
-    //        return PartialView("AdminMenu"); // Could add a model aswell.
-    //    }
-    //    [Authorize(Roles = "Employee, Admin")]
-    //    // GET: Admin
-    //    public ActionResult AdminMenu() // Make this Async task?
-    //    {
-    //        return PartialView("EmployeeMenu");
-    //    }
+    [Authorize(Roles = "Employee, Admin")]
+    public class AdminMenuController : Controller
+    {
+        // GET: Admin
 
-    //    public ActionResult AdminMenu()
-    //    {
-    //        return null;
-    //    }
-    //}
+        public async Task<ActionResult> AdminMenu() // Make this Async task?
+        {
+            if (User.IsInRole("Admin"))
+            {
+                return PartialView("AdminMenu"); // Could add a model aswell.
+            }
+            else if (User.IsInRole("Employee"))
+            {
+                return PartialView("EmployeeMenu");
+            }
+            else return null;
+        }
+    }
 }
