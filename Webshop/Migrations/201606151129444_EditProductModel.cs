4581bb73e7cfa7dd76091fe5c6f2489eca9a06cc
dewzor <bergla84@hotmail.com>
2016-06-15 16:22:34 +0200

    Update productimage half done

---
+++ Webshop/Migrations/201606151129444_EditProductModel.cs
@@ -0,0 +1,16 @@
+namespace Webshop.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class EditProductModel : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
---
+++ Webshop/Models/EditProduct.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Webshop.Models
+{
+    public class EditProduct
+    {
+        public int ProductId { get; set; }
+        public string Name { get; set; }
+        public Category Category { get; set; }
+        public decimal Price { get; set; }
+        public string ImageUrl { get; set; }
+        public string Description { get; set; }
+        public bool Published { get; set; }
+        public string CategoryName { get; set; } //Might not really be needed but its a workaround for now to get EditProduct form working.
+        public HttpPostedFileBase ProfileImage { get; set; }
+    }
+}
