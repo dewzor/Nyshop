92bdf819b4e234f275c539dc2cd7b713759e497d
dewzor <16418668+dewzor@users.noreply.github.com>
2016-06-24 01:10:31 +0200

    Api and design

---
+++ Webshop/Scripts/review.js
@@ -0,0 +1,17 @@
+﻿$('#sendreview').click(function () {
+    var text = $("#Comment").val();
+    var userid ='@User.Identity.Name';
+    var productitem = @Model.ProductId;
+    var review = {'comment' : text, 'customerid': userid, 'productid': productitem };
+
+    $.ajax({
+        url: "/api/review",
+        type: "POST",
+        data: JSON.stringify(review),
+        dataType: 'json',
+        contentType: "application/json; charset=utf-8",
+        success: function (data) { location.reload(); },
+        error: function () { alert("error"); }
+    });
+});
+
